82fd1335ca662646ffbfb78ccb8df5107dba72c8
Tania Mendez <tanita82_pmv@hotmail.com>
2013-04-21 12:33:38 -0400

    Bolivian Explorer Green
    Aplicacion Movil que explora los recursos renovables de Bolivia

---
+++ app - copia.js
@@ -0,0 +1,257 @@
+//<debug>
+Ext.Loader.setPath({
+    'Ext': 'js/src',
+    'Ext.plugin': 'js/map/lib/plugin'
+});
+//</debug>
+
+
+Ext.application({
+    startupImage: {
+        '320x460': 'js/map/resources/startup/Default.jpg', // Non-retina iPhone, iPod touch, and all Android devices
+        '640x920': 'js/map/resources/startup/640x920.png', // Retina iPhone and iPod touch
+        '640x1096': 'js/map/resources/startup/640x1096.png', // iPhone 5 and iPod touch (fifth generation)
+        '768x1004': 'js/map/resources/startup/768x1004.png', //  Non-retina iPad (first and second generation) in portrait orientation
+        '748x1024': 'js/map/resources/startup/748x1024.png', //  Non-retina iPad (first and second generation) in landscape orientation
+        '1536x2008': 'js/map/resources/startup/1536x2008.png', // : Retina iPad (third generation) in portrait orientation
+        '1496x2048': 'js/map/resources/startup/1496x2048.png' // : Retina iPad (third generation) in landscape orientation
+    },
+
+    isIconPrecomposed: false,
+    icon: {
+        57: 'js/map/resources/icons/icon.png',
+        72: 'js/map/resources/icons/icon@72.png',
+        114: 'js/map/resources/icons/icon@2x.png',
+        144: 'js/map/resources/icons/icon@144.png'
+    },
+
+    requires: [
+        'Ext.Map',
+        'Ext.Button',
+        'Ext.SegmentedButton',
+        'Ext.Panel',
+        'Ext.Toolbar',
+        'Ext.plugin.google.Traffic',
+        'Ext.plugin.google.Tracker'
+    ],
+
+    launch: function() {
+	
+		var txtLat, txtLong, cboMes;
+
+        txtLat = Ext.create('Ext.field.Text', {
+            name: 'lat',
+            placeHolder: 'Latitud'
+        });
+		
+		txtLong = Ext.create('Ext.field.Text', {
+            name: 'long',
+            placeHolder: 'Longitud'
+        });
+		
+        selectField = Ext.create('Ext.field.Select', {
+            name: 'mes',
+            options: [
+                { text: 'Enero',  value: '1' },
+                { text: 'Febrero', value: '2' },
+                { text: 'Marzo', value: '3' },
+                { text: 'Abril', value: '4' },
+                { text: 'Mayo', value: '5' },
+                { text: 'Junio', value: '6' },
+                { text: 'Julio', value: '7' },
+                { text: 'Agosto', value: '8' },
+                { text: 'Septiembre', value: '9' },
+                { text: 'Octubre', value: '10' },
+                { text: 'Noviembre', value: '11' },
+                { text: 'Diciembre', value: '12' }
+            ]
+        });
+       
+        var position = new google.maps.LatLng(37.44885, -122.158592),  
+
+            infowindow = new google.maps.InfoWindow({
+                content: ''
+            }),
+
+            
+            image = new google.maps.MarkerImage(
+                'js/map/resources/images/point.png',
+                new google.maps.Size(32, 31),
+                new google.maps.Point(0, 0),
+                new google.maps.Point(16, 31)
+            ),
+
+            shadow = new google.maps.MarkerImage(
+                'js/map/resources/images/shadow.png',
+                new google.maps.Size(64, 52),
+                new google.maps.Point(0, 0),
+                new google.maps.Point(-5, 42)
+            ),
+
+            trackingButton = Ext.create('Ext.Button', {
+                iconCls: 'locate'
+            }),
+
+            trafficButton = Ext.create('Ext.Button', {
+                pressed: true,
+                iconCls: 'maps'
+            }),
+
+            toolbar = Ext.create('Ext.Toolbar', {
+                docked: 'top',
+                ui: 'light',
+                items: [txtLat, txtLong, selectField, 	
+					
+					new Ext.Button({
+						text: 'Find',
+						handler: function() {
+							alert('poner marcador aqui');
+							refresh(this.mapdemo);     
+                        }
+					}),
+                    {
+                        iconCls: 'home',
+                        handler: function() {
+                            
+                            var segmented = Ext.getCmp('segmented'),
+                                pressedButtons = segmented.getPressedButtons(),
+                                trafficIndex = pressedButtons.indexOf(trafficButton),
+                                newPressed = (trafficIndex != -1) ? [trafficButton] : [];
+                            segmented.setPressedButtons(newPressed);
+                            mapdemo.getMap().panTo(position);
+                        }
+                    },
+                    {
+                        id: 'segmented',
+                        xtype: 'segmentedbutton',
+                        allowMultiple: true,
+                        listeners: {
+                            toggle: function(buttons, button, active) {
+                                if (button == trafficButton) {
+                                    mapdemo.getPlugins()[1].setHidden(!active);
+                                }
+                                else if (button == trackingButton) {
+                                    var tracker = mapdemo.getPlugins()[0],
+                                        marker = tracker.getMarker();
+                                    marker.setVisible(active);
+                                    if (active) {
+                                        tracker.setTrackSuspended(false);
+                                        Ext.defer(function() {
+                                            tracker.getHost().on('centerchange', function() {
+                                                marker.setVisible(true);
+                                                tracker.setTrackSuspended(true);
+                                                var segmented = Ext.getCmp('segmented'),
+                                                    pressedButtons = segmented.getPressedButtons(),
+                                                    trafficIndex = pressedButtons.indexOf(trafficButton),
+                                                    newPressed = (trafficIndex != -1) ? [trafficButton] : [];
+                                                segmented.setPressedButtons(newPressed);
+                                            }, this, {single: true});
+                                        }, 50, this);
+                                    }
+                                }
+                            }
+                        },
+                        items: [
+                            //trackingButton, trafficButton
+                        ]
+                    }
+                ]
+            });
+
+        var mapdemo = Ext.create('Ext.Map', {
+            mapOptions : {
+                center : new google.maps.LatLng(37.381592, -122.135672),  
+                zoom : 12,
+                mapTypeId : google.maps.MapTypeId.ROADMAP,
+                navigationControl: true,				
+				useCurrentLocation: true,
+
+                navigationControlOptions: {
+                    style: google.maps.NavigationControlStyle.DEFAULT
+                }
+            },
+
+            plugins : [
+                new Ext.plugin.google.Tracker({
+                    trackSuspended: false,   
+                    allowHighAccuracy: false,
+                    marker: new google.maps.Marker({
+                        position: position,
+                        title: 'Mi Posicion Actual',
+                        shadow: shadow,
+                        icon: image
+                    })
+                }),
+                new Ext.plugin.google.Traffic()
+            ],
+
+            listeners: {
+				centerchange : function(comp, map){
+                   refresh(map);
+                },
+                maprender: function(comp, map) {
+                    var marker = new google.maps.Marker({
+                        position: position,
+                        title : '',
+                        map: map
+                    });										
+
+                    google.maps.event.addListener(marker, 'click', function() {
+                        infowindow.open(map, marker);
+                    });
+					
+					/*
+                    setTimeout(function() {
+                        mapdemo.map.panTo(position);
+                    }, 1000);	
+					
+					
+					function addMarker(mappanel) {
+						
+							var lat = txtLat.getValue();
+							var lon = txtLong.getValue();
+						var map = mappanel.getMap(),
+						position = new google.maps.LatLng(lat, lon),
+						marker = new google.maps.Marker({
+						 position: position,                 
+						 //title : 'Sencha HQ',                 
+						// icon:'http://a3.twimg.com/profile_images/1089012240/sencha-logo_normal.png'
+						 });
+						 map.panTo(position);
+						 marker.setMap(map);
+					 } */
+                }
+            }
+        });
+		
+		refresh = function(theMap) {
+            var geoTag = {
+                lat: '47.584863',
+                longi: '-122.147026',
+                text: 'Hello World',
+            }
+            addMarker(geoTag, theMap);
+        }
+
+        addMarker = function(geoTag, theMap) {
+            var latLng = new google.maps.LatLng(geoTag.lat, geoTag.longi);
+            var marker = new google.maps.Marker({
+                map: theMap,
+                position: latLng
+            });
+			
+			google.maps.event.addListener(marker, "click", function() {
+                geoTagBubble.setContent(tweet.text);
+                geoTagBubble.open(theMap.map, marker);
+            });
+        };
+		
+		
+
+        Ext.create('Ext.Panel', {
+            fullscreen: true,
+            layout: 'fit',
+            items: [toolbar, mapdemo]
+        });
+    }
+});
